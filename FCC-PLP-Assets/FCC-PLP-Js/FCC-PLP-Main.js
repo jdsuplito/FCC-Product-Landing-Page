0454b2c0c38409d45229ef36b4a1e515ea87c7d9
JD <jdsuplito@gmail.com>
2021-05-30 23:29:59 +0800

    create initial layout

---
+++ FCC-PLP-Assets/FCC-PLP-Js/FCC-PLP-Main.js
@@ -0,0 +1,41 @@
+
+
+
+// STICKY NAVBAR
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("nav-bar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky-nav")
+  } else {
+    navbar.classList.remove("sticky-nav");
+  }
+}
+
+// SLIDESHOW AREA
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";
+}
